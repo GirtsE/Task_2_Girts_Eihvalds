bdcd9de6f9b2e480a8a068e48743e9421e234c9b
GirtsE <geihvalds@gmail.com>
2022-12-08 22:23:48 +0200

    Responsive top navbar

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,16 +1,23 @@
-const videoElement = document.querySelector('video');
-const playPauseButton = document.querySelector('button');
+function MobileMenu() {
+		var links = document.getElementById("links");
+		var navbar = document.getElementById("main_nav")
+		var menuIcon = document.getElementById("MENU")
 
-playPauseButton.addEventListener('click', () => {
-	playPauseButton.classList.toggle('playing');
-	if (playPauseButton.classList.contains('playing')) {
-		videoElement.play();
-	}
-	else {
-		videoElement.pause();
-	}
-});
 
-videoElement.addEventListener('ended', () => {
-	playPauseButton.classList.remove('playing');
-});
+		if(window.screen.width>1200){
+			if (links.style.display === "flex") {
+				navbar.style.height ="92.51px"
+				links.style.display = "none";
+				document.body.style.overflow = "scroll";
+				menuIcon.src="assets/img/MenuOpen.svg";
+			  }		
+			else {
+				navbar.style.height = "100vh"
+				links.style.display = "flex";
+				document.body.style.overflow = "hidden";
+				menuIcon.src="assets/img/MenuX.svg";
+			}
+
+		}	
+}
+
